fae9e03001cfe9a82c73e752ecf50fda6d20bf08
celosotomaior <marcelo.sotomaior@gmail.com>
2023-09-04 12:58:35 -0500

    - Deleted unnecessary key

--- ChaseWeatherApp/ChaseWeatherApp/Helpers/UserDefaultKeys.swift
+++ ChaseWeatherApp/ChaseWeatherApp/Helpers/UserDefaultKeys.swift
@@ -4,6 +4,5 @@ enum UserDefaultKeys {
     static let combineSwitcherKey = "combineSwitcherKey"
     static let hasLaunchedBefore = "hasLaunchedBefore"
     static let city = "cityName"
-    static var firstRequestInSessionKey = "firstRequestInSession"
     static var combineSwitcherValue = true // Set the initial value here
 }
